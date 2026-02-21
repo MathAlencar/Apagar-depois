54136a59ceaa2787b4ae444409bcddf470f1cff2
matheus nascimento <matheuskater02@outlook.com>
2026-02-21 19:43:08 -0300

    fix: resolvendo problema do ECMS 5

--- dist/methods/metodos.js
+++ dist/methods/metodos.js
@@ -1,9 +1,9 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; } function _nullishCoalesce(lhs, rhsFn) { if (lhs != null) { return lhs; } else { return rhsFn(); } } function _optionalChain(ops) { let lastAccessLHS = undefined; let value = ops[0]; let i = 1; while (i < ops.length) { const op = ops[i]; const fn = ops[i + 1]; i += 2; if ((op === 'optionalAccess' || op === 'optionalCall') && value == null) { return undefined; } if (op === 'access' || op === 'optionalAccess') { lastAccessLHS = value; value = fn(value); } else if (op === 'call' || op === 'optionalCall') { value = fn((...args) => value.call(lastAccessLHS, ...args)); lastAccessLHS = undefined; } } return value; }var _promises = require('fs/promises'); var _promises2 = _interopRequireDefault(_promises);
 var _path = require('path'); var _path2 = _interopRequireDefault(_path);
-var _url = require('url');
+// import { fileURLToPath } from "url";
 
-const __filename = _url.fileURLToPath.call(void 0, import.meta.url);
-const __dirname = _path2.default.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
  class Objetos{
 
