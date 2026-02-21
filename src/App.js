3391198c5043ad95784299b169e7030e8347c90d
matheus nascimento <matheuskater02@outlook.com>
2026-02-21 19:19:05 -0300

    fix: resolvendo problema do ECMS

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ import AdmintokenRoutes from './routes/administrador/TokenRoutes.js';
 import RpaRoutes from './routes/rpaRoutes.js';
 
 // __dirname em ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const PUBLIC_DIR = path.join(__dirname, '../Front-end/Front-end');
 const PUBLIC_DIR_UPLOAD = path.join(__dirname, '../Front-end/Documentos Excel/');
