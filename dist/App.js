626bc44a1d39d1e3c6803c332457b3aca05758c7
matheus nascimento <matheuskater02@outlook.com>
2026-02-21 19:23:24 -0300

    fix: resolvendo problema do ECMS 1

--- dist/App.js
+++ dist/App.js
@@ -1,6 +1,6 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _path = require('path'); var _path2 = _interopRequireDefault(_path);
-var _url = require('url');
+// import { fileURLToPath } from 'url';
 var _fs = require('fs'); var _fs2 = _interopRequireDefault(_fs);
 var _adminLoginRiqueredjs = require('./middlewares/adminLoginRiquered.js'); var _adminLoginRiqueredjs2 = _interopRequireDefault(_adminLoginRiqueredjs);
 
